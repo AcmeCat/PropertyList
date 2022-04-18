b484105555d2cfe1501c9207a0c3ad179995dca8
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2022-04-18 22:58:57 +0800

    data populates details view. images display. app icon added

--- app/src/main/java/com/illtech/propertylist/PropertyDetailsFragment.kt
+++ app/src/main/java/com/illtech/propertylist/PropertyDetailsFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import java.text.NumberFormat
 
 class PropertyDetailsFragment : Fragment () {
 
@@ -13,10 +18,41 @@ class PropertyDetailsFragment : Fragment () {
         fun newInstance () = PropertyDetailsFragment ()
     }
 
+    private lateinit var propertyDetailsViewModel: PropertyDetailsViewModel
+    private lateinit var property: Property
+
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
 
         var view = inflater.inflate(R.layout.fragment_details, container, false)
 
+        val context = activity as ViewModelStoreOwner
+        propertyDetailsViewModel = ViewModelProvider(context).get(PropertyDetailsViewModel::class.java)
+
+        property = propertyDetailsViewModel.selectedProperty.value!!
+
+        val addressText = view?.findViewById<TextView>(R.id.address)
+        addressText?.text = property.address
+
+        val priceText = view?.findViewById<TextView>(R.id.price)
+        priceText?.text = formatPrice(property.price)
+
+        val agentText = view?.findViewById<TextView>(R.id.agent)
+        agentText?.text = property.agent
+
+        val propertyImage = view?.findViewById<ImageView>(R.id.property_details_image)
+        val image = property.image
+        val resourceId = this.resources.getIdentifier(image, "drawable",
+            this.activity?.packageName
+        )
+        propertyImage?.setImageResource(resourceId)
+
         return view
+    }
+
+    //duplicate method with adaptor
+    fun formatPrice(price: Int) : String {
+        val moneyFormatter = NumberFormat.getCurrencyInstance()
+        moneyFormatter.maximumFractionDigits = 0
+        return moneyFormatter.format(price)
     }
 }
--- app/src/main/java/com/illtech/propertylist/PropertyListFragment.kt
+++ app/src/main/java/com/illtech/propertylist/PropertyListFragment.kt
@@ -19,12 +19,12 @@ class PropertyListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        propertyArray.add(Property("1", "house1.jpg", "7 Newhaven Street, Mount Tarcoola, 6530", 350000, "John Sudz"))
-        propertyArray.add(Property("2", "house1.jpg", "32B Mark Way, Mount Tarcoola, 6530", 180000, "Chelsea Wiles"))
-        propertyArray.add(Property("3", "house1.jpg", "2/138 Fitzgerald Street, Beachlands, 6530", 325000, "Marie Peters"))
-        propertyArray.add(Property("4", "house1.jpg", "13 Eastbourne Parade, Sunset Beach, 6530", 285000, "Pip Allen"))
-        propertyArray.add(Property("5", "house1.jpg", "58 Thomas Street, West Perth, 6005", 585000, "Carl Phathead"))
-        propertyArray.add(Property("6", "house1.jpg", "1/34 Thomas Street, West Perth, 6005", 760000, "Don Wan Sills"))
+        propertyArray.add(Property("1", "house1", "7 Newhaven Street, Mount Tarcoola, 6530", 350000, "John Sudz"))
+        propertyArray.add(Property("2", "house2", "32B Mark Way, Mount Tarcoola, 6530", 180000, "Chelsea Wiles"))
+        propertyArray.add(Property("3", "house3", "2/138 Fitzgerald Street, Beachlands, 6530", 325000, "Marie Peters"))
+        propertyArray.add(Property("4", "house4", "13 Eastbourne Parade, Sunset Beach, 6530", 285000, "Pip Allen"))
+        propertyArray.add(Property("5", "house5", "58 Thomas Street, West Perth, 6005", 585000, "Carl Phathead"))
+        propertyArray.add(Property("6", "house6", "1/34 Thomas Street, West Perth, 6005", 760000, "Don Wan Sills"))
     }
 
     override fun onCreateView(
